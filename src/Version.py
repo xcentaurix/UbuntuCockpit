d631ac68f0104f2cd86735279a62cd5750f5f1ad
xcentaurix <swmaniacster@gmail.com>
2026-04-07 20:07:06 +0000

    25.10

--- src/Version.py
+++ src/Version.py
@@ -21,6 +21,6 @@
 
 ID = "UBU"
 PLUGIN = "UbuntuCockpit"
-VERSION = "0.7.25"
+VERSION = "25.10"
 COPYRIGHT = "2018-2025 by xcentaurix"
 LICENSE = "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version."
