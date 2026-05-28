af59f8045390caae27e47cdd3414bf32d4122116
xcentaurix <swmaniacster@gmail.com>
2026-05-28 13:28:51 +0000

    25.10

--- src/plugin.py
+++ src/plugin.py
@@ -2,14 +2,18 @@
 # License: GNU General Public License v3.0 (see LICENSE file for details)
 
 
+import os
 from .Version import VERSION
 from .FileUtils import copyFile
+from .Debug import logger
 
 
 def Plugins(**__):
     logger.info("+++ Version: %s starts...", VERSION)
-    if not os.path.exists("/bin/ubuntu) {
-        copyFile("/usr/lib/enigma2/python/Plugins/SystemPlugins/bin/ubuntu", "/usr/bin/.")
-    }
+    if not os.path.exists("/bin/ubuntu"):
+        copyFile(
+            "/usr/lib/enigma2/python/Plugins/SystemPlugins/UbuntuCockpit/root/enter_chroot",
+            "/bin/ubuntu"
+        )
     descriptors = []
     return descriptors
